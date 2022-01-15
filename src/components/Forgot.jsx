ab16549b7c4e79952154c379910b3867cb88f70d
Eduardo <eduardoguette@gmail.com>
2022-01-15 19:13:04 +0100

    fix import useState

--- src/components/Forgot.jsx
+++ src/components/Forgot.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link, } from 'react-router-dom';
-import { useState } from 'react/cjs/react.development';
+import { Link} from 'react-router-dom'; 
 import { recoverPassword } from '../helpers';
 import { useForm } from '../hooks/useForm';
 
