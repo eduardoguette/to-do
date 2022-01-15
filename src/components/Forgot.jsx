6b5744bb06d09ca54cc234a46f1571c6a298f917
Eduardo <eduardoguette@gmail.com>
2022-01-15 23:06:21 +0100

    fix 404

--- src/components/Forgot.jsx
+++ src/components/Forgot.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link} from 'react-router-dom'; 
 import { recoverPassword } from '../helpers';
 import { useForm } from '../hooks/useForm';
