281cc84b493ebf95c37701e81ced8866ea676524
Eduardo <eduardoguette@gmail.com>
2022-01-23 19:05:01 +0100

    se corrigieron problemas de layout

--- src/components/Forgot.jsx
+++ src/components/Forgot.jsx
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from 'react-query';
 import { Link } from 'react-router-dom';
 import { recoverPassword } from '../helpers';
 import { useForm } from '../hooks/useForm';
+import imgLogo from "/img/logo.png"
 
 export const Forgot = () => {
   const queryClient = useQueryClient();
@@ -27,7 +28,7 @@ export const Forgot = () => {
 
   return (
     <>
-      <div className='absolute md:left-20 md:top-20 md:right-20 left-5 top-5 right-5  flex items-center justify-between'>
+      <div className=' md:absolute md:left-20 md:top-20 md:right-20 left-5 top-5 right-5 mb-10 flex items-center justify-between'>
         <Link to='/session/sign-in' className='font-semibold  bg-amaranth-400 text-white rounded-md p-1'>
           <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
             <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M15 19l-7-7 7-7' />
@@ -42,6 +43,9 @@ export const Forgot = () => {
         </p>
       </div>
       <div className='md:w-6/12'>
+      <Link to="/" className='md:hidden mb-10 block mx-auto'>
+          <img src={imgLogo} alt="Logo" width={120}/>
+        </Link>
         <h1 className='text-2xl mb-4 font-semibold'>Perdiste tu contraseña?</h1>
         <p className='mb-4'>Ingrese la dirección de correo electrónico que utilizó cuando se unió y le enviaremos instrucciones para restablecer su contraseña.</p>
         <form onSubmit={handleSubmit} className='flex flex-col'>
